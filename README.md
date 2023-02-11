da9d8dfdd3bdc9b0529cd57142aa43649deef9bb
Christophe Simonis <christophe@kn.gl>
2023-02-11 21:01:47 +0100

    Dave — Je me suis fait avoiner. Je me suis fait casser la gueule par un mec, une brute. Il voulait que je parle, mais j’ai rien dit du tout, j’ai pas dit où en était l’enquête, malgré la douleur. Peter — Encore heureux que tu aies pas dit où en était l’enquête, parce que vu qu’on en est au point zéro, si tu l’avais dit on passait pour des busards.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Aah... Monde de merde.
+
+git alias
+
+```
+abitbol = !curl -s http://abitbol.kn.gl | git commit -F -
+```
--- api/abitbol.py
+++ api/abitbol.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 import os
 import random
 
 from flask import Flask
-import simplejson
 
 HERE = os.path.dirname(__file__)
 app = Flask(__name__)
 
 with open(os.path.join(HERE, 'classe-americaine.json')) as ca:
-    quotes = simplejson.load(ca)
+    quotes = json.load(ca)
 
 @app.route('/')
 def index():
     q = random.choice(quotes)
     return (q, 200, [('content-type', 'text/plain; charset=utf-8')])
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
